b6a267f4bc8d3b3c9c6d8807122517ad4bdcf13d
Oliver Caldwell <oliver@flowdev.co.uk>
2012-02-08 21:08:19 +0000

    Added the practically blank main JavaScript file

---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+/*jshint smarttabs:true,mootools:true*/
+
+(function() {
+	'use strict';
+	
+	/**
+	 * Main function run when the DOM is ready
+	 */
+	function main() {
+		
+	}
+	
+	document.addEvent('domready', main);
+}());
