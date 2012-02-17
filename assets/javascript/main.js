9db2f3d8de1344b3ab9ad22221fb07a9ff9a4438
Oliver Caldwell <olliec87@gmail.com>
2012-02-17 21:28:25 +0000

    Added the base JavaScript. Sandboxing wrapper + DOM ready listener.

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+(function(exports) {
+	/*jshint mootools:true,smarttabs:true*/
+	'use strict';
+	
+	/**
+	 * Main function
+	 * Run when the DOM is ready
+	 */
+	function main() {
+		
+	}
+	
+	// Run main when the DOM is ready
+	document.addEvent('domready', main);
+}(this));
